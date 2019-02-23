c91993acdc8ed24834ece3761b5a86913875c4a8
Unknown <266128@via.dk>
2019-02-23 21:36:14 +0100

    hearts, rock triggers, skiing pole

---
+++ Skiing/Assets/Scripts/HeartScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartScript : MonoBehaviour {
+    public static int lifeNumber = 3;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (lifeNumber == 2)
+        {
+            heart1.GetComponent<Renderer>().enabled = false;
+        }else if(lifeNumber == 1)
+        {
+            heart1.GetComponent<Renderer>().enabled = false;
+            heart2.GetComponent<Renderer>().enabled = false;
+        }
+        else if (lifeNumber == 0)
+        {
+            heart1.GetComponent<Renderer>().enabled = false;
+            heart2.GetComponent<Renderer>().enabled = false;
+            heart3.GetComponent<Renderer>().enabled = false;
+        }
+	}
+}
---
+++ Skiing/Assets/Scripts/Rocks_Trigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocks_Trigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        HeartScript.lifeNumber -= 1;
+    }
+}
