6d8c0d2514a9896bb6f8c77944b49940adb627bd
Unknown <266128@via.dk>
2019-02-25 19:34:51 +0100

    background music , vr controller creation

---
+++ Skiing/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public Rigidbody camera;
+
+    public float movementSpeed = 5;
+    public float rotationSpeed = 5;
+    public float rotationDegrees = 90;
+
+    // Use this for initialization
+    void Start () {
+        camera = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        camera.position += transform.forward * 1 * Time.deltaTime * movementSpeed;
+
+    }
+}
---
+++ Skiing/Assets/Scripts/SoundsForWinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsForWinner : MonoBehaviour {
+    public AudioClip win;
+    public AudioSource source;
+
+ void OnTriggerEnter(Collider other)
+    {
+        source.PlayOneShot(win);
+    }
+}
